c8790a401bf73ef4fc886aff597ba7bf6f9c6674
李永飞 <1334051004@qq.com>
2017-11-08 17:02:13 +0800

    初始化AR文件

---
+++ StarsSwift/项目/消息/会话/AR/YFARViewController.swift
@@ -0,0 +1,21 @@
+//
+//  YFARViewController.swift
+//  StarsSwift
+//
+//  Created by 李永飞 on 2017/11/8.
+//  Copyright © 2017年 李永飞. All rights reserved.
+//
+
+import UIKit
+
+class YFARViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+     
+
+}
